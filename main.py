f8c1300cfe21c50757da0d59f46abf81e1a0b785
Dopamine-Max <123927607+Dopamine-Max@users.noreply.github.com>
2023-04-18 16:49:07 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 #INITIAL SETUP
 #----------------------------------------------------------------
 import cv2 as cv
-import os
 from cvzone.HandTrackingModule import HandDetector
-from cvzone import overlayPNG
 import numpy as np
 import math
 import time
+import random
 
 intro = cv.imread('imgs/0.jpg')
 kill = cv.imread('imgs/1.png')
@@ -15,13 +14,27 @@ cam = cv.VideoCapture(1) #read the camera
 cam.set(3, 1920)
 cam.set(4, 1080)
 #sets the minimum confidence threshold for the detection
-detector = HandDetector(maxHands=1,detectionCon=0.77)
+detector = HandDetector(maxHands=1,detectionCon=0.8)
 
 #INITIALIZING GAME COMPONENTS
 #----------------------------------------------------------------
 sqr = cv.imread('imgs/sqr.png')
-ox, oy = 375, 100
+img_canvas = np.zeros((1080,1920,3),np.uint8)
+
 tipIds = [4,8,12,16,20]
+curr_x1, curr_y1 = 0,0
+prev_x1, prev_y1 = 0,0
+x1,y1 = [],[]
+distance_drawn = 0
+
+gameOver = False
+NotWon = True
+toStartGame = False
+toStartTime = False
+
+TIMER_MAX=60
+TIMER=TIMER_MAX
+prev=time.time()
  
 #INTRO SCREEN WILL STAY UNTIL Q IS PRESSED
 cv.imshow('Squid Game',cv.resize(intro,(0,0),fx=0.69,fy=0.69))
@@ -33,29 +46,28 @@ while True:
 
 #GAME LOGIC UPTO THE TEAMS
 #-----------------------------------------------------------------------------------------
-gameOver = False
-NotWon = True
-while not gameOver:
+
+while not gameOver and TIMER>=0:
+    
     ret,img = cam.read()
     img = cv.resize(img,(1920,1080))
     img = cv.flip(img, 1)
     img = cv.addWeighted(sqr,0.5,img,0.5,0)
     hands,img = detector.findHands(img)
-    img_c = img.copy()
     
-    cv.line(img, (851,426), (851,652), (26,67,102), thickness=22)
+    """ cv.line(img, (851,426), (851,652), (26,67,102), thickness=22)
     cv.line(img, (851,426), (1077,426), (26,67,102), thickness=22)
     cv.line(img, (1077,426), (1077,652), (26,67,102), thickness=22)
-    cv.line(img, (851,652), (1077,652), (26,67,102), thickness=22)
+    cv.line(img, (851,652), (1077,652), (26,67,102), thickness=22) """
     
     #Starting point
-    cv.circle(img, (851,426),15,(255,0,0),-1)
+    cv.circle(img, (851,427),13,(255,0,0),-1)
     
-    
-    if hands:
+    if len(hands) != 0:
         fingers = []
         lmList = hands[0]['lmList']
         cursor = lmList[8]
+        curr_x1, curr_y1 = cursor[0], cursor[1]
             
         for id in range(1,5):
             if lmList[tipIds[id]][2] < lmList[tipIds[id]-2][2]:
@@ -64,16 +76,60 @@ while not gameOver:
                 fingers.append(0)
         
         if fingers[1] and fingers[2] == False:
-            cv.circle(img, (cursor[0],cursor[1]),15,(255,0,255),cv.FILLED)
+            
+            starting_pt_dist = math.sqrt((curr_x1 - 851)**2 + (curr_y1 - 426)**2)
         
-        if (851>cursor[0]>873 or 1055>cursor[0]>1077) and (426>cursor[1]>404 or 630>cursor[1]>652):
-            print("Inside")
+            if starting_pt_dist < 5:
+                toStartGame = True
+                toStartTime = True
             
+            if toStartGame:
+                x1.append(curr_x1)
+                y1.append(curr_y1)
+                
+                if prev_x1 == 0 and prev_y1 == 0:
+                    prev_x1, prev_y1 = curr_x1,curr_y1
+                
+                cv.line(img,(prev_x1,prev_y1),(curr_x1, curr_y1),(255,0,0),20)
+                cv.line(img_canvas,(prev_x1,prev_y1),(curr_x1, curr_y1),(255,0,0),20)
+                
+                prev_x1,prev_y1 = curr_x1,curr_y1
             
+                if len(x1) > 2 and len(y1) > 2:
+                    distance_drawn += math.sqrt((curr_x1 - x1[-2])**2 + (curr_y1 - y1[-2])**2)
+
+            """ if (851>cursor[0]>1077 and 427>cursor[1]>426) or (1078>cursor[0]>1077 and 426>cursor[1]>652) or (853>cursor[0]>851 and 654>cursor[1]>652) or (851>cursor[0]>1077 and 652>cursor[1]>426):
+                pass
+            else:
+                gameOver = True """
+        
+            if starting_pt_dist < 5 and distance_drawn > (4*225):
+                toStartGame = False
+                NotWon = False
+                gameOver = True
+        
+            if toStartTime:
+                cv.putText(img,str(TIMER),(50,50),cv.FONT_HERSHEY_SIMPLEX,1,(0,int(255*(TIMER)/TIMER_MAX),int(255*(TIMER_MAX-TIMER)/TIMER)),4,cv.LINE_AA)
+                cur=time.time()
+                no=random.randint(1,5)
+                if cur-prev>=no:
+                    prev=cur
+                    TIMER=TIMER-no
+                
+    img = cv.addWeighted(img,0.5,img_canvas,0.5,0)    
     cv.imshow('Squid Game', cv.resize(img,(0,0),fx=0.69,fy=0.69))
     if cv.waitKey(10) & 0xFF == ord('q'):
             break
+        
+else:
+    if NotWon:
+        while True:
+            #show the loss screen incase time runs out
+            cv.imshow('Squid Game',cv.resize(kill,(0,0),fx=0.69,fy=0.69))
+            if cv.waitKey(10) & 0xFF == ord('q'):
+                break
     
+cam.release()
 
 #LOSS SCREEN
 if NotWon:
